25c47116c0626d2f63175e63ee9e6b28af89e0fe
MoKi <Mokiwins@gmail.com>
2023-04-03 13:42:48 -0400

    i++

--- src/main/java/ArrayExercise.java
+++ src/main/java/ArrayExercise.java
@@ -7,8 +7,8 @@ public class ArrayExercise {
   }
   
   public void incrementArray(int[] input) {
-    for(int i = 0; i < input.length; i++) {
-      input[i] = input[i] + 1;
+    for (int i = 0; i < input.length; i++) {
+      input[i]++;
     }
   }
 
